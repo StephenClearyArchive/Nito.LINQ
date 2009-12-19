ff8e723f18074f432f1dd9da8cbece0262cc3233
shammah_cp <SND\shammah_cp>
2009-12-19 18:32:30 +0000

    Added CF 3.5 project.

---
+++ trunk/Source/CF35.NoRx/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyDescription("Nito.LINQ for .NET Compact Framework 3.5 without Rx")]
+[assembly: AssemblyTitle("Nito.LINQ for .NET Compact Framework 3.5 without Rx")]
