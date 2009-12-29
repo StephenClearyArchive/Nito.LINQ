b8defd6e60c8dd4ff2d11929e6753e92096cb02a
Stephen Cleary <StephenCleary@users.noreply.github.com>
2009-12-29 15:04:40 +0000

    Renamed LexicographicalComparer to SequenceComparer.

--- trunk/Source/UnitTests.Sources/SequenceComparerUnitTests.cs
+++ trunk/Source/UnitTests.Sources/SequenceComparerUnitTests.cs
@@ -10,74 +10,74 @@ namespace UnitTests
     public partial class Tests
     {
         [TestMethod]
-        public void LexicographicalComparer_EmptySequences_ReturnsEqual()
+        public void SequenceComparer_EmptySequences_ReturnsEqual()
         {
             var x = new int[] { };
             var y = new int[] { };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result == 0, "Empty sequences should be equal.");
         }
 
         [TestMethod]
-        public void LexicographicalComparer_FirstSequenceEmpty_ReturnsLessThan()
+        public void SequenceComparer_FirstSequenceEmpty_ReturnsLessThan()
         {
             var x = new int[] { };
             var y = new int[] { 1 };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result < 0, "Empty sequence should be less than non-empty sequence.");
         }
 
         [TestMethod]
-        public void LexicographicalComparer_SecondSequenceEmpty_ReturnsGreaterThan()
+        public void SequenceComparer_SecondSequenceEmpty_ReturnsGreaterThan()
         {
             var x = new int[] { 1 };
             var y = new int[] { };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result > 0, "Empty sequence should be less than non-empty sequence.");
         }
 
         [TestMethod]
-        public void LexicographicalComparer_EqualSequences_ReturnsEqual()
+        public void SequenceComparer_EqualSequences_ReturnsEqual()
         {
             var x = new int[] { 1, 2, 3 };
             var y = new int[] { 1, 2, 3 };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result == 0, "Equal sequences should be equal.");
         }
 
         [TestMethod]
-        public void LexicographicalComparer_FirstSequenceShorter_ReturnsLessThan()
+        public void SequenceComparer_FirstSequenceShorter_ReturnsLessThan()
         {
             var x = new int[] { 1, 2 };
             var y = new int[] { 1, 2, 3 };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result < 0, "Shorter sequence should be less than longer sequence.");
         }
 
         [TestMethod]
-        public void LexicographicalComparer_SecondSequenceShorter_ReturnsGreaterThan()
+        public void SequenceComparer_SecondSequenceShorter_ReturnsGreaterThan()
         {
             var x = new int[] { 1, 2, 3 };
             var y = new int[] { 1, 2 };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result > 0, "Shorter sequence should be less than longer sequence.");
         }
 
         [TestMethod]
-        public void LexicographicalComparer_FirstSequenceSmaller_ReturnsLessThan()
+        public void SequenceComparer_FirstSequenceSmaller_ReturnsLessThan()
         {
             var x = new int[] { 1, 1, 3 };
             var y = new int[] { 1, 2, 3 };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result < 0, "Smaller sequence should be less than larger sequence.");
         }
 
         [TestMethod]
-        public void LexicographicalComparer_SecondSequenceSmaller_ReturnsGreaterThan()
+        public void SequenceComparer_SecondSequenceSmaller_ReturnsGreaterThan()
         {
             var x = new int[] { 1, 1, 3 };
             var y = new int[] { 1, 0, 3 };
-            int result = new LexicographicalComparer<int>().Compare(x, y);
+            int result = new SequenceComparer<int>().Compare(x, y);
             Assert.IsTrue(result > 0, "Smaller sequence should be less than larger sequence.");
         }
     }
