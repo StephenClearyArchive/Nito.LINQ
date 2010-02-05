4572e2ee8c5a8b347c4a5e3c3db24aee8589a6a1
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-02-05 23:34:07 +0000

    Added Nito.Assimilation.

---
+++ trunk/Assimilation/assimilate/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Cci;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.Cci.MutableCodeModel;
+
+namespace assimilate
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (Debugger.IsAttached)
+            {
+                return ProgramMain(args);
+            }
+            else
+            {
+                try
+                {
+                    return ProgramMain(args);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: [" + ex.GetType().Name + "] " + ex.Message + ex.StackTrace);
+                    return -1;
+                }
+            }
+        }
+
+        static int ProgramMain(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                return Usage();
+            }
+
+            switch (args[0])
+            {
+                case "meta":
+                    if (args.Length != 3)
+                    {
+                        return Usage();
+                    }
+
+                    return GenerateMetadataAssembly(args[1], args[2]);
+
+                default:
+                    return Usage();
+            }
+        }
+
+        static int Usage()
+        {
+            Console.WriteLine("Usage: assimilate <command> <arguments>");
+            Console.WriteLine("Commands:");
+            Console.WriteLine("  meta <existing assembly> <new assembly>");
+            Console.WriteLine("    Generates a metadata assembly from an existing assembly");
+            return -1;
+        }
+
+        static int GenerateMetadataAssembly(string originalAssembly, string metadataAssembly)
+        {
+            var host = new PeReader.DefaultHost();
+
+            var module = host.LoadUnitFrom(originalAssembly) as IModule;
+            if (module == null || module == Dummy.Module || module == Dummy.Assembly)
+            {
+                Console.WriteLine(originalAssembly + " is not a .NET assembly");
+            }
+
+            module = StripToMetadata.Run(host, module);
+            using (var peStream = File.Create(metadataAssembly))
+            {
+                PeWriter.WritePeToStream(module, host, peStream);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ trunk/Assimilation/assimilate/Util/MetadataExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Cci;
+
+namespace assimilate
+{
+    public static class MetadataExtensions
+    {
+        public static bool IsExposed(this ITypeDefinitionMember member)
+        {
+            return member.Visibility != TypeMemberVisibility.Private && member.Visibility != TypeMemberVisibility.Other &&
+                member.Visibility != TypeMemberVisibility.FamilyAndAssembly && member.Visibility != TypeMemberVisibility.Assembly;
+        }
+    }
+}
