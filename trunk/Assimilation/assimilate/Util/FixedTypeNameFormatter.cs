f596bc4e98654dd6a0329c2fa514296ea3b7e188
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-02-08 12:17:51 +0000

    Duplicated some TypeNameFormatter code so that when they fix their bugs, it won't mess up our bug fixes. :)

--- trunk/Assimilation/assimilate/Util/FixedTypeNameFormatter.cs
+++ trunk/Assimilation/assimilate/Util/FixedTypeNameFormatter.cs
@@ -8,55 +8,136 @@ namespace assimilate
 {
     public sealed class FixedTypeNameFormatter : TypeNameFormatter
     {
-        // Fixed: honors NameFormattingOptions.MemberKind
-        public override string GetNamespaceName(IUnitNamespaceReference unitNamespace, NameFormattingOptions formattingOptions)
+        /// <summary>
+        /// Returns a C#-like string that corresponds to the given referenced namespace definition and that conforms to the specified formatting options.
+        /// Fixes the base method by honoring <see cref="NameFormattingOptions.DocumentationIdMemberKind"/> and <see cref="NameFormattingOptions.MemberKind"/>.
+        /// </summary>
+        /// <param name="unitNamespaceReference">The namespace reference whose name is returned.</param>
+        /// <param name="formattingOptions">The options for formatting.</param>
+        /// <returns>The namespace name.</returns>
+        public override string GetNamespaceName(IUnitNamespaceReference unitNamespaceReference, NameFormattingOptions formattingOptions)
         {
+            string ret = this.GetNamespaceNameWithoutKind(unitNamespaceReference, formattingOptions);
             if ((formattingOptions & NameFormattingOptions.DocumentationIdMemberKind) != 0)
             {
-                return "N:" + base.GetNamespaceName(unitNamespace, formattingOptions & ~NameFormattingOptions.DocumentationIdMemberKind & ~NameFormattingOptions.MemberKind);
+                ret = "N:" + ret;
             }
             else if ((formattingOptions & NameFormattingOptions.MemberKind) != 0)
             {
-                return "namespace " + base.GetNamespaceName(unitNamespace, formattingOptions & ~NameFormattingOptions.MemberKind);
+                ret = "namespace " + ret;
             }
-            else
-            {
-                return base.GetNamespaceName(unitNamespace, formattingOptions);
-            }
+
+            return ret;
         }
 
-        // Fixed: honors NameFormattingOptions.MemberKind
+        /// <summary>
+        /// Returns a C#-like string that corresponds to the given unit set namespace definition and that conforms to the specified formatting options.
+        /// Fixes the base method by honoring <see cref="NameFormattingOptions.DocumentationIdMemberKind"/> and <see cref="NameFormattingOptions.MemberKind"/>.
+        /// </summary>
+        /// <param name="namespaceDefinition">The namespace definition whose name is returned.</param>
+        /// <param name="formattingOptions">The options for formatting.</param>
+        /// <returns>The namespace name.</returns>
         public override string GetNamespaceName(IUnitSetNamespace namespaceDefinition, NameFormattingOptions formattingOptions)
         {
+            string ret = this.GetNamespaceNameWithoutKind(namespaceDefinition, formattingOptions);
             if ((formattingOptions & NameFormattingOptions.DocumentationIdMemberKind) != 0)
             {
-                return "N:" + base.GetNamespaceName(namespaceDefinition, formattingOptions & ~NameFormattingOptions.DocumentationIdMemberKind & ~NameFormattingOptions.MemberKind);
+                ret = "N:" + ret;
             }
             else if ((formattingOptions & NameFormattingOptions.MemberKind) != 0)
             {
-                return "namespace " + base.GetNamespaceName(namespaceDefinition, formattingOptions & ~NameFormattingOptions.MemberKind);
+                ret = "namespace " + ret;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Returns a C#-like string that corresponds to the given type definition and that conforms to the specified formatting options.
+        /// Fixes the base method by honoring <see cref="NameFormattingOptions.DocumentationIdMemberKind"/> and <see cref="NameFormattingOptions.MemberKind"/> correctly.
+        /// </summary>
+        /// <param name="nsType">The namespace type reference whose name is returned.</param>
+        /// <param name="formattingOptions">The options for formatting; <see cref="NameFormattingOptions.DocumentationIdMemberKind"/> and <see cref="NameFormattingOptions.MemberKind"/> are ignored.</param>
+        /// <returns>A possibly-qualified type name.</returns>
+        protected override string GetNamespaceTypeName(INamespaceTypeReference nsType, NameFormattingOptions formattingOptions)
+        {
+            string ret = this.GetNamespaceTypeNameWithoutKind(nsType, formattingOptions);
+            if ((formattingOptions & NameFormattingOptions.DocumentationIdMemberKind) != 0)
+            {
+                ret = "T:" + ret;
+            }
+            else if ((formattingOptions & NameFormattingOptions.MemberKind) != 0)
+            {
+                ret = this.GetTypeKind(nsType) + " " + ret;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Gets a possibly-qualified namespace name. Identical to TypeNameFormatter.GetNamespaceName at the time of writing (duplicated to prevent their bug fixes from messing up our bug fixes).
+        /// </summary>
+        /// <param name="unitNamespaceReference">The namespace reference whose name is returned.</param>
+        /// <param name="formattingOptions">The options for formatting; <see cref="NameFormattingOptions.DocumentationIdMemberKind"/> and <see cref="NameFormattingOptions.MemberKind"/> are ignored.</param>
+        /// <returns>A possibly-qualified namespace name.</returns>
+        private string GetNamespaceNameWithoutKind(IUnitNamespaceReference unitNamespaceReference, NameFormattingOptions formattingOptions)
+        {
+            INestedUnitNamespaceReference nestedUnitNamespace = unitNamespaceReference as INestedUnitNamespaceReference;
+            if (nestedUnitNamespace == null)
+            {
+                // Root namespace
+                return string.Empty;
+            }
+
+            if (nestedUnitNamespace.ContainingUnitNamespace is IRootUnitNamespaceReference || (formattingOptions & NameFormattingOptions.OmitContainingNamespace) != 0)
+            {
+                return nestedUnitNamespace.Name.Value;
             }
             else
             {
-                return base.GetNamespaceName(namespaceDefinition, formattingOptions);
+                return this.GetNamespaceNameWithoutKind(nestedUnitNamespace.ContainingUnitNamespace, formattingOptions) + "." + nestedUnitNamespace.Name.Value;
             }
         }
 
-        // Fixed: honors NameFormattingOptions.DocumentationIdMemberKind and NameFormattingOptions.MemberKind
-        protected override string GetNamespaceTypeName(INamespaceTypeReference nsType, NameFormattingOptions formattingOptions)
+        /// <summary>
+        /// Gets a possibly-qualified namespace name. Identical to TypeNameFormatter.GetNamespaceName at the time of writing (duplicated to prevent their bug fixes from messing up our bug fixes).
+        /// </summary>
+        /// <param name="namespaceDefinition">The namespace definition whose name is returned.</param>
+        /// <param name="formattingOptions">The options for formatting; <see cref="NameFormattingOptions.DocumentationIdMemberKind"/> and <see cref="NameFormattingOptions.MemberKind"/> are ignored.</param>
+        /// <returns>A possibly-qualified namespace name.</returns>
+        private string GetNamespaceNameWithoutKind(IUnitSetNamespace namespaceDefinition, NameFormattingOptions formattingOptions)
         {
-            if ((formattingOptions & NameFormattingOptions.DocumentationIdMemberKind) != 0)
+            INestedUnitSetNamespace nestedUnitSetNamespace = namespaceDefinition as INestedUnitSetNamespace;
+            if (nestedUnitSetNamespace == null)
             {
-                return "T:" + base.GetNamespaceTypeName(nsType, formattingOptions & ~NameFormattingOptions.DocumentationIdMemberKind & ~NameFormattingOptions.MemberKind);
+                return namespaceDefinition.Name.Value;
             }
-            else if ((formattingOptions & NameFormattingOptions.MemberKind) != 0)
+
+            if (nestedUnitSetNamespace.ContainingNamespace.Name.Value.Length == 0 || (formattingOptions & NameFormattingOptions.OmitContainingNamespace) != 0)
             {
-                return this.GetTypeKind(nsType) + " " + base.GetNamespaceTypeName(nsType, formattingOptions & ~NameFormattingOptions.MemberKind);
+                return nestedUnitSetNamespace.Name.Value;
             }
             else
             {
-                return base.GetNamespaceTypeName(nsType, formattingOptions);
+                return this.GetNamespaceNameWithoutKind(nestedUnitSetNamespace.ContainingUnitSetNamespace, formattingOptions) + "." + nestedUnitSetNamespace.Name.Value;
             }
+        }
+
+        /// <summary>
+        /// Gets a possibly-qualified name for a namespace type reference. Similar to TypeNameFormatter.GetNamespaceTypeName (duplicated to prevent their bug fixes from messing up our bug fixes).
+        /// </summary>
+        /// <param name="nsType">The namespace type reference whose name is returned.</param>
+        /// <param name="formattingOptions">The options for formatting; <see cref="NameFormattingOptions.DocumentationIdMemberKind"/> and <see cref="NameFormattingOptions.MemberKind"/> are ignored.</param>
+        /// <returns>A possibly-qualified type name.</returns>
+        private string GetNamespaceTypeNameWithoutKind(INamespaceTypeReference nsType, NameFormattingOptions formattingOptions)
+        {
+            string ret = this.AddGenericParametersIfNeeded(nsType, nsType.GenericParameterCount, formattingOptions, nsType.Name.Value);
+            if ((formattingOptions & NameFormattingOptions.OmitContainingNamespace) == 0 && !(nsType.ContainingUnitNamespace is IRootUnitNamespaceReference))
+            {
+                ret = this.GetNamespaceNameWithoutKind(nsType.ContainingUnitNamespace, formattingOptions) + "." + ret;
+            }
+
+            return ret;
         }
     }
 }
