21620cc9051d5b968674866e51afcd5907a64fe7
shammah_cp <SND\shammah_cp>
2010-02-09 14:34:23 +0000

    Added simple test library.

---
+++ trunk/Assimilation/Test/Tests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class TestGlobalClass
+{
+}
+
+public struct TestGlobalStruct
+{
+}
+
+public delegate void TestGlobalDelegate();
+
+namespace Test
+{
+    public delegate void TestNamespaceDelegate();
+
+    public class TestNamespaceClass
+    {
+        public delegate void TestNestedDelegate();
+
+        public event Action TestEvent;
+
+        public int TestProperty { get; set; }
+
+        public int TestField;
+
+        public void TestMethod() { }
+
+        public static void TestStaticMethod() { }
+
+        public class TestNestedClass
+        {
+        }
+
+        public struct TestNestedStruct
+        {
+        }
+    }
+
+    namespace TestNestedNamespace
+    {
+        public delegate void TestNamespaceDelegate();
+
+        public class TestNamespaceClass
+        {
+            public delegate void TestNestedDelegate();
+
+            public event Action TestEvent;
+
+            public int TestProperty { get; set; }
+
+            public int TestField;
+
+            public void TestMethod() { }
+
+            public static void TestStaticMethod() { }
+
+            public class TestNestedClass
+            {
+            }
+
+            public struct TestNestedStruct
+            {
+            }
+        }
+    }
+}
