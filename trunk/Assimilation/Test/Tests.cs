9231511711332def6042e4079832fcc1806ebf54
shammah_cp <SND\shammah_cp>
2010-03-13 19:49:38 +0000

    Added more test objects.

--- trunk/Assimilation/Test/Tests.cs
+++ trunk/Assimilation/Test/Tests.cs
@@ -13,6 +13,16 @@ public struct TestGlobalStruct
 
 public delegate void TestGlobalDelegate();
 
+public class TestGenericGlobalClass<T>
+{
+}
+
+public struct TestGenericGlobalStruct<T>
+{
+}
+
+public delegate void TestGenericGlobalDelegate<T>(T arg);
+
 namespace Test
 {
     public delegate void TestNamespaceDelegate();
@@ -63,6 +73,59 @@ namespace Test
             }
 
             public struct TestNestedStruct
+            {
+            }
+        }
+    }
+
+    public delegate void TestGenericNamespaceDelegate<T>();
+
+    public class TestGenericNamespaceClass<T>
+    {
+        public delegate void TestGenericNestedDelegate<R>();
+
+        public event Action TestEvent;
+
+        public int TestProperty { get; set; }
+
+        public int TestField;
+
+        public void TestGenericMethod<R>() { }
+
+        public static void TestGenericStaticMethod<R>() { }
+
+        public class TestGenericNestedClass<R>
+        {
+        }
+
+        public struct TestGenericNestedStruct<R>
+        {
+        }
+    }
+
+    namespace TestNestedNamespace
+    {
+        public delegate void TestGenericNamespaceDelegate<T>();
+
+        public class TestGenericNamespaceClass<T>
+        {
+            public delegate void TestGenericNestedDelegate<R>();
+
+            public event Action TestEvent;
+
+            public int TestProperty { get; set; }
+
+            public int TestField;
+
+            public void TestGenericMethod<R>() { }
+
+            public static void TestGenericStaticMethod<R>() { }
+
+            public class TestGenericNestedClass<R>
+            {
+            }
+
+            public struct TestGenericNestedStruct<R>
             {
             }
         }
