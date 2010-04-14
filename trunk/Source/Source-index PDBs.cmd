d33026c8f554f048f27c50041fbb1a1eaf570981
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-04-14 19:51:57 +0000

    Updated binary output directories to go to the Binaries folder.

--- trunk/Source/Source-index PDBs.cmd
+++ trunk/Source/Source-index PDBs.cmd
@@ -1,8 +1,8 @@
 @echo off
 REM This file requires Debugging Tools for Windows, Perl (e.g., ActivePerl), and svn.exe (e.g., CollabNet/Tigris)
-call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=35.NoRx\bin\Release /Debug
-call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=35.Rx\bin\Release /Debug
-call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=40.NoRx\bin\Release /Debug
-call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=SL3.NoRx\bin\Release /Debug
-call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=SL3.Rx\bin\Release /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\35.NoRx /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\35.Rx /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\40.NoRx /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\SL3.NoRx /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\SL3.Rx /Debug
 pause
