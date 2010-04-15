f622300c0a8398a30ea1bb3578affbba88936290
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-04-15 19:18:56 +0000

    Added project for .NET 4.0 with Rx, and updated EnumerableExtensions for Rx 1.0.2441.0.

---
+++ trunk/Source/UnitTests.Rx/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnitTests.Rx")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microline Technology Corporation")]
+[assembly: AssemblyProduct("UnitTests.Rx")]
+[assembly: AssemblyCopyright("Copyright © Microline Technology Corporation 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("978b5fc9-06ab-4afb-8c17-8f1cf4e7b237")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
