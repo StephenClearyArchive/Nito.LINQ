a01395563a2b220beb8d76f959ab82e67a0cb062
Stephen Cleary <StephenCleary@users.noreply.github.com>
2010-06-14 20:37:10 +0000

    Added SL4 projects to the source indexing command.

--- trunk/Source/Source-index PDBs.cmd
+++ trunk/Source/Source-index PDBs.cmd
@@ -6,4 +6,6 @@ call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SY
 call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\40.Rx /Debug
 call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\SL3.NoRx /Debug
 call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\SL3.Rx /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\SL4.NoRx /Debug
+call "c:\Program Files\Debugging Tools for Windows (x86)\srcsrv\ssindex.cmd" /SYSTEM=SVN /SYMBOLS=..\Binaries\SL4.Rx /Debug
 pause
